b8794b3ba9b80dddc450f2ce7f8f49290ee4a966
Thiago Coppi <coppithiago@gmail.com>
2021-03-12 08:28:40 -0300

    Update choco_setup.ps1

--- choco_setup.ps1
+++ choco_setup.ps1
@@ -1,4 +1,4 @@
-$Packages = 'nodejs-lts', 'jdk8', 'vscode', 'visualstudio2019community', 'spotify', 'winmerge', 'dbeaver', 'slack', 'winscp', 'wsl', 'wsl-ubuntu-1804', 'drawio', 'git', 'tortoisegit', '7zip', 'docker-desktop', 'notepadplusplus', 'sharex', 'office365proplus', 'microsoft-teams', 'postman'
+$Packages = 'nodejs-lts', 'jdk8', 'vscode', 'visualstudio2019community', 'spotify', 'winmerge', 'dbeaver', 'slack', 'winscp', 'wsl', 'wsl-ubuntu-1804', 'drawio', 'git', 'tortoisegit', '7zip', 'docker-desktop', 'notepadplusplus', 'sharex', 'office365proplus', 'microsoft-teams', 'postman', 'discord'
 
 ForEach ($PackageName in $Packages)
 {
